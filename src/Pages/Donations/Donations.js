b8707c58596d1c3bcca944ae47a5b8839f310d28
ifty03 <ashikulislamb84@gmail.com>
2022-04-28 15:20:03 +0600

    Message

--- src/Pages/Donations/Donations.js
+++ src/Pages/Donations/Donations.js
@@ -9,6 +9,12 @@ const Donations = () => {
       .then((data) => setDonations(data));
   }, []);
 
+  useEffect(() => {
+    fetch("", {
+      method: "DELETE",
+    });
+  }, []);
+
   return (
     <div className="grid lg:grid-cols-2 grid-cols-1 md:w-4/6 w-5/6 gap-8 mx-auto my-10">
       {donations.map((donation) => (
